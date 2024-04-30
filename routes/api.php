507c37e7e3c085f703ed706df0b4b0c37d1f0e60
ezelamar <ezequiel.lamarquee@gmail.com>
2024-04-30 11:59:40 -0300

    Laravel Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\PlayerController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/players', [PlayerController::class, 'index']);
